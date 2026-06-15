d834a9dc18c2921253a44a85eca2693cb663d2e5
thaua-david <thauadavid@gmail.com>
2026-06-15 19:40:17 -0300

    Atualizando Connection.hs para deploy híbrido

--- app/Database/Connection.hs
+++ app/Database/Connection.hs
@@ -2,12 +2,24 @@
 module Database.Connection (obterConexao) where
 
 import Database.PostgreSQL.Simple
+import System.Environment (lookupEnv)
+import Data.ByteString.Char8 (pack)
 
--- Função simples para conectar no banco
 obterConexao :: IO Connection
-obterConexao = connect defaultConnectInfo
-    { connectHost     = "localhost"
-    , connectDatabase = "wiki_garcia"
-    , connectUser     = "postgres"
-    , connectPassword = "thauaadm123" -- IMPORTANTE: Coloque a senha do seu pgAdmin aqui!
-    }
+obterConexao = do
+    -- O Haskell procura a variável de ambiente da nuvem
+    envUrl <- lookupEnv "DATABASE_URL"
+    
+    case envUrl of
+        Just url -> do
+            putStrLn "Iniciando em MODO PRODUÇÃO (Banco na Nuvem)..."
+            connectPostgreSQL (pack url)
+            
+        Nothing -> do
+            putStrLn "Iniciando em MODO DESENVOLVIMENTO (Banco Local)..."
+            connect defaultConnectInfo 
+                { connectHost     = "localhost"
+                , connectDatabase = "wiki_garcia"
+                , connectUser     = "postgres"
+                , connectPassword = "thauaadm123" 
+                }
